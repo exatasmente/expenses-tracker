18d903a6fbf8395ee55f11df890bb57a6dc8b720
Luiz <luiznetodev@gmail.com>
2025-01-26 20:20:26 -0300

    add manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,17 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Lar Control - Gestão de Contas',
   description: 'Lar Control - Gestão de Contas',
+  viewport: 'width=device-width, initial-scale=1',
+  manifest: '/manifest.json',
+  publisher: 'Lar Control',
+  creator: 'Lar Control',
+  applicationName: 'Lar Control',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-icon.png',
+    
+  }
 }
 
 export default function RootLayout({
